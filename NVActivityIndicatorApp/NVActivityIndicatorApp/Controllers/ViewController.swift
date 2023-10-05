9dedadc2eae2e6422cb6160ec5f33a2954fc8991
FilovetsAlexandr <sohan.boxan@gmail.com>
2023-10-05 13:41:00 +0300

    project done with SPM

---
+++ NVActivityIndicatorApp/NVActivityIndicatorApp/Controllers/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  NVActivityIndicatorApp
+//
+//  Created by Alexandr Filovets on 5.10.23.
+//
+
+import NVActivityIndicatorView
+import NVActivityIndicatorViewExtended
+import UIKit
+
+final class ViewController: BaseViewController {
+    
+    @IBAction func startButtonAction() { startAnimation() }
+}
